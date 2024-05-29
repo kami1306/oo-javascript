b48599aff66458572a48189cc53ff1d48edd7b41
kami1306 <162999455+kami1306@users.noreply.github.com>
2024-05-29 08:34:03 -0300

    Add files via upload

---
+++ alterar.js
@@ -0,0 +1,14 @@
+const Pessoa ={
+    nome: "Luma",
+    profissao: "Engenheira",
+};
+
+console.log(Pessoa.telefone)
+
+Pessoa.telefone = "41 9999-9999"
+
+console.log(Pessoa.telefone);
+
+Pessoa.nome = "Luma Silva";
+
+console.log(Pessoa)
