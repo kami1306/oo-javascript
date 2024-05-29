8df0c9a0466edb879038b3e04f88afd14dbc1c89
kami1306 <162999455+kami1306@users.noreply.github.com>
2024-05-29 08:33:28 -0300

    Add files via upload

---
+++ objetos.js
@@ -0,0 +1,16 @@
+const objetosPessoa = {
+    nome: "Jose" ,
+    idade: 32
+}; 
+
+const cliente = {
+    nome: "Andre",
+    idade:32,
+    cpf:"111222333",
+    email:"andre!@gmail.com"
+};
+
+console.log(`O nome do cliente é ${cliente.nome}
+ e essa pessoa tem ${cliente.idade} anos.`);
+console.log(`Os tres primeiros digitos do CPF são ${cliente.cpf.substring(0,3)}`);
+
