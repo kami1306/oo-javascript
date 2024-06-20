851db3079f768671dbd4b188703f16a1ceeb51ae
kami1306 <162999455+kami1306@users.noreply.github.com>
2024-06-20 08:37:53 -0300

    Add files via upload

---
+++ aninhados.js
@@ -0,0 +1,17 @@
+const cliente = {
+    nome :"João",
+    idade: 24,
+    email:"joao@firma.com",
+    telefone:["(41) 9999-0000","(41) 9988-0000"],
+};
+
+cliente.endereco = {
+    rua: "R. Joseph Climber",
+    numero :true,
+    complemento : "Ap 984",
+
+};
+
+console.log(cliente);
+console.log(cliente.endereco);
+console.log(cliente["endereco"]);
---
+++ listaObjetos.js
@@ -0,0 +1,16 @@
+const cliente = {
+    nome :"João",
+    idade: 24,
+    email:"joao@firma.com",
+    telefone:["(41) 9999-0000","(41) 9988-0000"],
+};
+
+cliente.endereco = [
+    {
+    rua: "R. Joseph Climber",
+    numero :true,
+    complemento : "Ap 984",
+
+},
+];
+
